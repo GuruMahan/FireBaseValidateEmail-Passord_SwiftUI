6043ec0d358af66ab769fcc6c9904a7ad125041b
GuruMahan <iosdeve2022@gmail.com>
2023-02-07 15:35:12 +0530

    Initial Commit

---
+++ FireBaseValidateEmail$Passord/FireBaseValidateEmail_PassordApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireBaseValidateEmail_PassordApp.swift
+//  FireBaseValidateEmail$Passord
+//
+//  Created by Guru Mahan on 07/02/23.
+//
+
+import SwiftUI
+
+@main
+struct FireBaseValidateEmail_PassordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
